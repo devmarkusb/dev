aec46369bf41a77f222ca4a4491ece94d6ad6d1f
MarkusB <>
2023-02-15 11:12:06 +0100

    fix prime-sieve graph

--- apps/experiments/from_mathematics_to_generic_programming/src/prime-sieve.cpp
+++ apps/experiments/from_mathematics_to_generic_programming/src/prime-sieve.cpp
@@ -7,42 +7,45 @@ namespace
 {
 constexpr int index(int value)
 {
+    UL_EXPECT(value >= 3);
     return (value - 3) / 2;
-}
-
-int pi(int n, const std::vector<uint8_t>& primes)
-{
-    UL_EXPECT(index(n) <= ul::narrow_cast<int>(primes.size()));
-    int count{};
-    for (int i{}; i < index(n); ++i)
-    {
-        UL_ASSERT(i >= 0);
-        if (primes[static_cast<size_t>(i)])
-        {
-            ++count;
-        }
-    }
-    return count;
 }
 } // namespace
 
 int main()
 {
-    constexpr auto m{100'000};
+    constexpr auto m{10'000'000};
     constexpr auto mi{index(m)};
     std::vector<uint8_t> primes;
     primes.resize(mi);
     math::sift2(std::begin(primes), mi);
 
-    std::vector<double> x(m);
-    std::vector<double> y(m);
-    for (int i{}; i < m; ++i)
+    std::vector<int> x(m);
+    std::vector<int> y(m);
+    std::vector<int> yfit(m);
+    x[0] = 0;
+    x[1] = 1;
+    x[2] = 2;
+    y[0] = 0;
+    y[1] = 0;
+    y[2] = 1;
+    int pi{1};
+    for (int i{3}; i < m; ++i)
     {
+        if ((i & 0x1) && primes[static_cast<size_t>(index(i))])
+        {
+            ++pi;
+        }
         x[static_cast<size_t>(i)] = i;
-        y[static_cast<size_t>(i)] = pi(i, primes);
+        y[static_cast<size_t>(i)] = pi;
+        yfit[static_cast<size_t>(i)] = 1./16. * i + 40000;
     }
 
+    matplotlibcpp::title("Number of primes < x");
     matplotlibcpp::named_plot("pi(x)", x, y);
+    matplotlibcpp::named_plot("fit(x)", x, yfit);
+    matplotlibcpp::legend();
+    matplotlibcpp::show();
 
     return ul::prog_exit_success;
 }
