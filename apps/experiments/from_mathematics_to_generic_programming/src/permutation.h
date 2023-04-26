515e2908cd6475a94b58493972b55fbda4a860ae
MarkusB <>
2023-04-26 21:13:51 +0200

    permutation

--- apps/experiments/from_mathematics_to_generic_programming/src/permutation.h
+++ apps/experiments/from_mathematics_to_generic_programming/src/permutation.h
@@ -23,7 +23,7 @@ std::pair<I0, I1> swap_ranges_n(I0 first0, I1 first1, N n)
 {
     while (n != N(0))
     {
-        swap(*first0++, *first1++);
+        std::swap(*first0++, *first1++);
         --n;
     }
     return {first0, first1};
@@ -50,6 +50,34 @@ void gries_mills_rotate(I f, I m, I l)
         p = swap_ranges(f, m, m, l);
     }
     // u == v
+}
+
+// Just working, not sure if that's the expected solution.
+template <std::forward_iterator I>
+void gries_mills_rotate_n(I f, I m, I l)
+{
+    auto u = std::distance(f, m);
+    auto v = std::distance(m, l);
+    if (u == 0 || v == 0)
+        return;
+    std::pair<I, I> p = swap_ranges_n(f, m, std::min(u, v));
+    while (u != v)
+    {
+        if (u < v)
+        {
+            f = m;
+            m = p.second;
+            v = v - u;
+        }
+        else
+        {
+            UL_ASSERT(v < u);
+            f = p.first;
+            u = u - v;
+        }
+        p = swap_ranges_n(f, m, std::min(u, v));
+    }
+    UL_ASSERT(u == v);
 }
 } // namespace math
 
--- apps/experiments/from_mathematics_to_generic_programming/src/permutation.test.cpp
+++ apps/experiments/from_mathematics_to_generic_programming/src/permutation.test.cpp
@@ -4,7 +4,34 @@
 
 TEST(gries_mills_rotate, tests)
 {
-    auto arr{std::array{0, 1, 2, 3, 4}};
+    constexpr auto arr_orig{std::array{0, 1, 2, 3, 4}};
+    auto arr{arr_orig};
+    math::gries_mills_rotate(std::begin(arr), std::begin(arr), std::end(arr));
+    EXPECT_EQ(arr, arr_orig);
+    math::gries_mills_rotate(std::begin(arr), std::end(arr), std::end(arr));
+    EXPECT_EQ(arr, arr_orig);
     math::gries_mills_rotate(std::begin(arr), std::begin(arr) + 2, std::end(arr));
-    EXPECT_EQ(arr, std::array{2, 3, 4, 0, 1});
+    auto arr_expected{std::array{2, 3, 4, 0, 1}};
+    EXPECT_EQ(arr, arr_expected);
+    arr = arr_orig;
+    math::gries_mills_rotate(std::begin(arr), std::begin(arr) + 4, std::end(arr));
+    arr_expected = std::array{4, 0, 1, 2, 3};
+    EXPECT_EQ(arr, arr_expected);
+}
+
+TEST(gries_mills_rotate_n, tests)
+{
+    constexpr auto arr_orig{std::array{0, 1, 2, 3, 4}};
+    auto arr{arr_orig};
+    math::gries_mills_rotate_n(std::begin(arr), std::begin(arr), std::end(arr));
+    EXPECT_EQ(arr, arr_orig);
+    math::gries_mills_rotate_n(std::begin(arr), std::end(arr), std::end(arr));
+    EXPECT_EQ(arr, arr_orig);
+    math::gries_mills_rotate_n(std::begin(arr), std::begin(arr) + 2, std::end(arr));
+    auto arr_expected{std::array{2, 3, 4, 0, 1}};
+    EXPECT_EQ(arr, arr_expected);
+    arr = arr_orig;
+    math::gries_mills_rotate_n(std::begin(arr), std::begin(arr) + 4, std::end(arr));
+    arr_expected = std::array{4, 0, 1, 2, 3};
+    EXPECT_EQ(arr, arr_expected);
 }
