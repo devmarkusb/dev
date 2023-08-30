d3cd3366456b92d94023becc67ba068e3053a8b1
MarkusB <>
2023-08-30 10:40:22 +0200

    tmp

--- apps/tmp-cleansheet/src/main.cpp
+++ apps/tmp-cleansheet/src/main.cpp
@@ -1,5 +1,26 @@
 #include "util/allthethings.h"
 
+template<class T, class... U>
+concept AnyOf = (std::same_as<T, U> || ...);
+
+struct S {
+    int i{};
+};
+
+template<typename T>
+concept ContextC = AnyOf<T, int, S>;
+
+
+int f(const ContextC auto& t) {
+    if constexpr (std::is_same_v<decltype(t), S>) {
+        return t.i;
+    } else {
+        return {};
+    }
+}
+
 int main(int, char*[]) {
+    f(42);
+    f(S{2});
     return 0;
 }
