c53a9ad9fc3a93015d15d4dd3fffd8cdb392e804
MarkusB <>
2023-08-30 18:46:52 +0200

    Revert "tmp"
    This reverts commit d3cd3366456b92d94023becc67ba068e3053a8b1.
    
    Revert "tmp"
    
    This reverts commit e7cabb85dcc57d5bd7bdc645ce47e0f231da08bf.

--- apps/tmp-cleansheet/src/main.cpp
+++ apps/tmp-cleansheet/src/main.cpp
@@ -1,23 +1,5 @@
 #include "util/allthethings.h"
 
-struct S {
-    int i{};
-};
-
-template<typename T>
-concept ContextC = ul::AnyOf<T, int, S>;
-
-
-int f(const ContextC auto& t) {
-    if constexpr (std::is_same_v<decltype(t), S>) {
-        return t.i;
-    } else {
-        return {};
-    }
-}
-
 int main(int, char*[]) {
-    f(42);
-    f(S{2});
     return 0;
 }
