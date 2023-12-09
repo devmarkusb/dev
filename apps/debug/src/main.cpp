370e612c38c92e4adf4ce6e9b009b83a374ed8f4
MarkusB <>
2023-12-09 23:56:35 +0100

    fill app for debugging experiments

--- apps/debug/src/main.cpp
+++ apps/debug/src/main.cpp
@@ -1,10 +1,32 @@
 #include "args.h"
+#include "ul/ul.h"
+
+namespace {
+const size_t some_nr{5};
+
+void f(bool& crash, size_t& nr) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    if (++nr == some_nr) {
+        crash = true;
+    }
+}
+}
 
 int main(int argc, char** argv) {
     const auto [args, exitCode]{Args::parse_command_line(argc, argv)};
 
     if (!args) {
         return exitCode;
+    }
+
+    bool crash{};
+
+    size_t nr{0};
+    while (true) {
+        f(crash, nr);
+        if (crash) {
+            ul::crash(SIGABRT);
+        }
     }
 
     return 0;
